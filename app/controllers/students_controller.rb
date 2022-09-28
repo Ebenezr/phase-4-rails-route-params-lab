a8324eeaaa427a3e276a8979aa311875c9f9227f
Ebenezr <ebenezarbukosia@gmail.com>
2022-09-28 10:50:35 +0300

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    
-    students = 
+    student = 
     if params[:name] 
-        Student.find_by_last_name(params[:name]) || Student.find_by_first_name(params[:name])  
+      Student.where("last_name LIKE ? OR first_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%") 
     else
        Student.all
     end  
-     
-    render json: students, only: [:first_name, :last_name, :grade] 
+    render json: student, except: [:created_at, :updated_at, :id]
   end
   
   def show
