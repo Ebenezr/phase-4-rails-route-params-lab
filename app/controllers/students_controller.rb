639ceab1ff3b77f729d05246d9d5ca6ddd6f5d95
Ebenezr <ebenezarbukosia@gmail.com>
2022-09-29 10:09:25 +0300

    commit tree 0e7d086898732e5beb69b2bf5e111294d7041341

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,22 @@
 class StudentsController < ApplicationController
-
-  def index
-    student = 
-    if params[:name] 
-      Student.where("last_name LIKE ? OR first_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%") 
-    else
-       Student.all
-    end  
-    render json: student, except: [:created_at, :updated_at, :id]
-  end
   
-  def show
-    student = Student.find(params[:id])
-    render json: student
+  def index
+    students =
+        if params[:name]
+          Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%","%#{params[:name]}%")
+        else
+          Student.all
+        end
+        render json: students , methods: :to_s 
   end
 
+  def show
+    student = Student.find_by_id( params[:id])
+    if student
+      render json: student, except: [:created_at, :updated_at], methods: :to_s
+    else  
+      render json: {error: 'Student not found'}, status: :not_found
+    end  
+  end
 
 end
